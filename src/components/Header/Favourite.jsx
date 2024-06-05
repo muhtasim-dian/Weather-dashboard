23c212a04ec2ec13cc1d493680211b2ff1616fa7
muhtasim-dian <muhtasim.dian@gmail.com>
2024-06-05 20:11:17 +0600

    Changing for Favourites right padding

--- src/components/Header/Favourite.jsx
+++ src/components/Header/Favourite.jsx
@@ -1,14 +1,14 @@
 import HeartImg from "../../assets/heart.svg";
 export default function Favourite({ onShow }) {
   return (
-    <div className=" sm:p-0  md:p-2 hover:bg-black/30 cursor-pointer flex gap-2 right-0 rounded-md transition-all ">
+    <div className="max-md:p-2 hover:bg-black/30 cursor-pointer flex items-start gap-2  rounded-md transition-all ">
       <img
         src={HeartImg}
         alt=""
       />
       <span
         onClick={onShow}
-        className="max-md:text-xs  max-md:w-full">
+        className="max-md:text-xs  px-2">
         Favourites
       </span>
     </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,10 +8,10 @@ export default function Header() {
   const [showFavModal, setShowFavModal] = useState(false);
   return (
     <header className="fixed w-full top-0 z-50 bg-gradient-to-b from-black/60 to-black/0 pb-10">
-      <nav className="container flex items-center justify-between sm:px-2 md:px-20 py-6">
+      <nav className="container flex items-center justify-between max-sm:px-2 md:px-20 py-6">
         <Logo />
 
-        <div className="flex items-center gap-4 relative">
+        <div className="flex items-center max-md:gap-4 sm:gap-1 relative">
           <Search />
           <Favourite
             onShow={() => {
